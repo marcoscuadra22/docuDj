ad1a5bad3682afd967e37248d616f53972191a63
marcoscuadra22 <cuadracarmonamarcos@gmail.com>
2024-03-25 14:45:31 +0100

    Termiando el error 404

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import HttpResponse, render
+from django.http import Http404
+from django.shortcuts import HttpResponse, render, get_object_or_404
 from django.template import loader
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    #template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
@@ -21,6 +19,11 @@ def results(request, question_id):
     
 def vote(request, question_id):
     return HttpResponse("You are voting on question %s." % question_id)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
 
 
 """
