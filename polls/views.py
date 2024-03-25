7870ad056442bc362fe29b4f4bb825a82c5f869a
marcoscuadra22 <cuadracarmonamarcos@gmail.com>
2024-03-25 10:40:01 +0100

    Modificacion de Views y añadido las Urls

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import HttpResponse
 
 def index(request):
+    lastest_question_list = Question.onjects.order_by("-pub_date"[:5])
+    output = ", ".join([q.question_text for q in lastest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+    
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
+
+
+
+
+"""
+Esta documentado porque es un simple hola mundo
+
+def index(request):
+
     return HttpResponse("Hola mundo")
+"""
