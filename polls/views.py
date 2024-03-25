5166ec313e98855c90438d9b02d5774409c118af
marcoscuadra22 <cuadracarmonamarcos@gmail.com>
2024-03-25 13:42:53 +0100

    No me detecta el templates

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
+from django.template import loader
+from .models import Question
+
 
 def index(request):
-    lastest_question_list = Question.onjects.order_by("-pub_date"[:5])
-    output = ", ".join([q.question_text for q in lastest_question_list])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    #template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s." % question_id)
@@ -14,8 +21,6 @@ def results(request, question_id):
     
 def vote(request, question_id):
     return HttpResponse("You are voting on question %s." % question_id)
-
-
 
 
 """
