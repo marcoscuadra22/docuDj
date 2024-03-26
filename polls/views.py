4e17007a9b794ef8e4f59d6ec1dff20ddb6901ac
marcoscuadra22 <cuadracarmonamarcos@gmail.com>
2024-03-26 11:01:09 +0100

    Terminada la def de vote falta añadir el aprtado de resultados

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,10 @@
-from django.http import Http404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import HttpResponse, render, get_object_or_404
 from django.template import loader
-from .models import Question
+from django.db.models import F
+from django.urls import reverse
+
+from .models import Question, Choice
 
 
 def index(request):
@@ -24,12 +27,21 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
-
-
-"""
-Esta documentado porque es un simple hola mundo
-
-def index(request):
-
-    return HttpResponse("Hola mundo")
-"""
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render (
+            request,
+            "polls/detail.html",
+            {
+              "question": question,
+              "error_message": "You didn't select a choice",  
+            },
+        )
+    else:
+        selected_choice.votes = f("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reserve("polls:results", args=(question.id,)))
+    
